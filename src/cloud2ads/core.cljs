7fcce692a0f57167fa9cf85ee09df3bd31d94c5e
Damien Gonot <damien.gonot@gmail.com>
2022-04-07 16:49:24 -0400

    initial commit

---
+++ src/cloud2ads/core.cljs
@@ -0,0 +1,19 @@
+(ns cloud2ads.core
+    (:require
+      [reagent.core :as r]
+      [reagent.dom :as d]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Welcome to cloud2ads"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
